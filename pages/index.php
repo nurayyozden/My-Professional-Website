dca2459f36a63d39c044281b8387b1d47d25aad3
Nuray Ozden <nyo3@cornell.edu>
2024-04-10 19:33:31 +0000

    adding router, json pages, first pages and organization

---
+++ pages/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <!-- <link rel="stylesheet" type="text/css" href="public/styles/site.css" /> -->
+  <title>Hi, I'm, Nuray</title>
+</head>
+
+<body>
+
+<header>
+    <h1>Welcome!</h1>
+</header>
+
+</body>
+</html>
