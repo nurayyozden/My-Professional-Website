4a8f31999685280f8a7aec11f07352fa5a1e13e9
Nuray Ozden <nyo3@cornell.edu>
2024-04-10 19:37:41 +0000

    images and styling

--- pages/index.php
+++ pages/index.php
@@ -4,14 +4,14 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <!-- <link rel="stylesheet" type="text/css" href="public/styles/site.css" /> -->
-  <title>Hi, I'm, Nuray</title>
+  <link rel="stylesheet" type="text/css" href="public/styles/site.css" />
+  <title>Home</title>
 </head>
 
 <body>
 
 <header>
-    <h1>Welcome!</h1>
+    <h1>Hi, I'm Nuray</h1>
 </header>
 
 </body>
